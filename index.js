7beac246373d933d7ec38ad552f4e9a78281c65d
ivBell <bellivan64@gmail.com>
2021-04-19 23:29:14 +0400

    filename

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(cors())
 app.use(express.json()) //добавляем чтение json
 app.use(express.static('static'))
 app.use(fileUpload({}))
+
 //Routes
 app.use('/api', CategoriesRouter)
 app.use('/api', ProductsRouter)
--- services/file.service.js
+++ services/file.service.js
@@ -4,8 +4,7 @@ import path from 'path'
 class FileService {
   saveJPG(file, name) {
     try {
-      const newName = name + uuid.v4()
-      const fileName = newName + '.jpg'
+      const fileName = name + '.jpg'
       const filePath = path.resolve('static/images', fileName)
       file.mv(filePath)
       return fileName
