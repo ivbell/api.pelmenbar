90b97df4676cf1c6177398737d40cd484dd27f3d
ivBell <bellivan64@gmail.com>
2021-04-19 14:23:17 +0400

    update slug img url

--- services/file.service.js
+++ services/file.service.js
@@ -4,7 +4,7 @@ import path from 'path'
 class FileService {
   saveJPG(file, name) {
     try {
-      const fileName = name + '.jpg'
+      const fileName = name + uuid.v4() + '.jpg'
       const filePath = path.resolve('static/images', fileName)
       file.mv(filePath)
       return fileName
