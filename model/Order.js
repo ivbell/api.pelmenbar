98bb028cdbcdc840e842852330157c0f40f726fb
ivBell <bellivan64@gmail.com>
2021-05-03 16:03:23 +0400

    reopen unix

--- model/Order.js
+++ model/Order.js
@@ -1,15 +1,16 @@
 import mongoose from 'mongoose'
 
 const Order = new mongoose.Schema({
-  product: {type: String, required: true},
-  delivery: {type: String, required: true},
-  phone: {type: String, required: true},
-  name: {type: String, required: true},
-  adress: {type: String, required: true},
-  sum: {type: String, required: true},
-  status: {type: Boolean, default: false},
-  onDelete: {type: Boolean, default: false},
-  date: {type: Date, default: Date.now},
+	product: { type: String, required: true },
+	delivery: { type: String, required: true },
+	phone: { type: String, required: true },
+	name: { type: String, required: true },
+	adress: { type: String, required: true },
+	sum: { type: String, required: true },
+	payment: { type: String, required: true },
+	status: { type: Boolean, default: false },
+	onDelete: { type: Boolean, default: false },
+	date: { type: Date, default: Date.now },
 })
 
 export default mongoose.model('Order', Order)
